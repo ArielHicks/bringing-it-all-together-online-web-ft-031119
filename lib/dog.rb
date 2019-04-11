e10551993aada667734f63230f3c3464ffe114f9
Ariel Hicks <hicks.ariel@gmail.com>
2019-04-11 16:52:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,39 +3,39 @@
 
 class Dog
 
-  attr_accessor :id, :name, :breed
+    attr_accessor :id, :name, :breed
 
-  def initialize(id: nil, name: "Fido", breed: "lab")
-    @id = nil
-    @name = name
-    @breed = breed
-  end
+    def initialize(id: nil, name: "Fido", breed: "lab")
+      @id = nil
+      @name = name
+      @breed = breed
+    end
 
-  def self.create_table
-    sql = <<-SQL
-      CREATE TABLE IF NOT EXISTS dogs (
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        breed TEXT
-      )
+    def self.create_table
+      sql = <<-SQL
+        CREATE TABLE IF NOT EXISTS dogs (
+          id INTEGER PRIMARY KEY,
+          name TEXT,
+          breed TEXT
+        )
+        SQL
+      DB[:conn].execute(sql)
+    end
+
+    def self.drop_table
+      sql = "DROP TABLE IF EXISTS dogs"
+      DB[:conn].execute(sql)
+    end
+
+    def save
+      if self.id
+        self.update
+      else sql = <<-SQL
+      INSERT INTO dogs (name, breed)
+      VALUES (?, ?)
       SQL
-    DB[:conn].execute(sql)
-  end
-
-  def self.drop_table
-    sql = "DROP TABLE IF EXISTS dogs"
-    DB[:conn].execute(sql)
-  end
-
-  def save
-    if self.id
-      self.update
-    else sql = <<-SQL
-    INSERT INTO dogs (name, breed)
-    VALUES (?, ?)
-    SQL
-    DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
-  end
+      DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
   self
   end
 
