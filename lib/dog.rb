475b1d66ed58f5724a97b8968b2c0386cf3a127e
Ariel Hicks <hicks.ariel@gmail.com>
2019-04-11 16:47:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,30 @@
+# require_relative "..config/environment.rb"
+
+
 class Dog
 
-attr_accessor :id, :name, :breed
+  attr_accessor :id, :name, :breed
+
+  def initialize(id: nil, name: "Fido", breed: "lab")
+    @id = nil
+    @name = name
+    @breed = breed
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+      SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS dogs"
+    DB[:conn].execute(sql)
+  end
 
 end
